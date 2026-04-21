c08a4e51b303bd8382b4c1cff1453da4d8d3a8c9
user971038 <A00226997@tec.mx>
2026-04-21 05:07:04 -0600

    remove flex display end or wtvr

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// adding notes until it will let me commit...
+// adding notes until it will let me commit... pt 2
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
