a36d3331195a28ca665e0c072d891304c0ddbdce
user971038 <A00226997@tec.mx>
2026-04-20 20:08:29 -0600

    add, move

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
 
 export const socket = io(URL, {
     auth: {
